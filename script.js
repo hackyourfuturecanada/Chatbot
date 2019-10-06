16706c1809bf76b0562c48783d8baff1fb45d7d7
terratamo <yunusunsal.efl@gmail.com>
2019-10-06 17:27:25 -0400

    ioChatbotweek11

--- script.js
+++ script.js
@@ -1,65 +1,88 @@
-const ioChatbot = [ {
-    input : "hello",
-    output : ["Hello", "Hi", "Greetings"]
- },
- {
-    input : "What is your favorite color?",
-    output : ["i am not sure", "There are too many", "i like everyone",]
- },
- {
-    input : "How are you",
-    output : ["fine", "great", "not so good"]
- }
+const ioChatbot= [
+    {
+        input:  ['hello', 'hi', 'greetings'],
+        output: ['Hello', 'Hey', 'Greetings']
+    },
+    {
+        input:  ['what is your favourite colour?', 'who is your favourite HYF instructor?', 'who is your role model?'],
+        output: ['I am not sure.','There are too many to choose from', 'I like every one']
+    },
+    {
+        input:  ['how are you?', 'how is the weather today?', 'how is Canada doing in the Olympics?'],
+        output: ['Fine', 'Great', 'Not so good']
+    },
+ 
+];
 
-]
 
+console.log(ioChatbot)
 
-
-console.log(ioChatbot);
-
+//for function 
 function reply() {
 
-    let question = document.getElementById("input").value.tolowerCase();
-
-    let randomNumber = Math.floor (Math.random()*3)
-
-    let be = document.getElementById("output").value
-
-    let filterType = null
-
-    let response = ioChatbot.filter(item => {return item.input === question } )
-
-    if (response.length>1){
-
-     if (document.getElementById("longest").checked == true ){
-
-             be= response[0].output[2];
+    let question = document.getElementById("input").value.toLowerCase();
     
-        } else if (document.getElementById("shortest").checked == true ){
+    const response = ioChatbot.filter( item => item.input.includes(question))
 
-            be = response[0].output[0];
-
-        } else {
-
-            be = response[0].output[randomNumber];
-    }
-
-
-    }else {
-
-            be = "i do not understand that comment. Please enter another!"
-
-        }
+// for value 0 to 2 
+    let randomNumber = Math.floor(Math.random()*3);
    
-    
+// for longest , shortest , random responses 
+    if(response.length>0){
 
+        if(document.getElementById('shortest').checked === true){
 
+            document.getElementById("output").value +="you :  "+question+ '\n' + "computer :  "+ response[0].output.sort((a, b) => a.length - b.length)[0]+ '\n' +'\n';
 
+        }else if (document.getElementById('longest').checked === true){
+          
+          
+            document.getElementById("output").value +="you :  "+question+ '\n'+"computer :  "+response[0].output.sort((a, b) => b.length - a.length)[response.length-1]+ '\n' +'\n';
+         
+        }else {
+          
+          
+            document.getElementById("output").value +="you :  "+question+ '\n' +"computer :  "+ response[0].output.sort((a, b) => b.length - a.length)[randomNumber]+ '\n' + '\n';
+        }
 
+    } else if( question === "show me a dog" ) {
+        dogpictures();
+
+        
+    } else if(question==='set an alarm') {
+        delayedAlert();
+        
+    } else {
+        document.getElementById("output").value="I do not understand that comment. Please enter another."
+    }  
 }
 
-//document.getElementById("submit").addEventListener("click",function() {reply()});
+document.getElementById("submit").addEventListener("click", function() {reply()});
 
-document.querySelector("#submit").addEventListener("click", function(){
-    reply();
- });
+
+
+function dogpictures(){
+
+   let image = new XMLHttpRequest();
+
+    image.onreadystatechange=function(){ 
+        if(image.readyState===4){
+    let url= JSON.parse(image.responseText).message;
+    
+    document.getElementById("dog").setAttribute('src', url);
+        }
+
+    }
+	    image.open("GET", "https://dog.ceo/api/breeds/image/random"); 
+        image.send();
+    
+}
+
+
+
+   function delayedAlert(){
+   
+        setTimeout(function(){
+                alert("Did you forget about me? it's your friend, the Alarm!")
+             }, 2000);
+    }
