b1f66fb9f77b56d9aeefbe19314cb583de60ac91
Umit Ilhan <umitilhan@Umits-MacBook-Pro-2.local>
2019-10-06 10:07:43 -0400

    chatbot 4

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,3 @@
-console.log("he");
 // Input and output contents declared
 const chatbotIO = [
     {
@@ -12,30 +11,46 @@ const chatbotIO = [
     {
         input : ['how are you?', 'how is the weather today?', 'how is canada doing in the olympics?'],
         output : ["Great", "Cool", "Good"]
-    }]
+    },
+    {
+        input : 'show me a dog',
+        output: ['dog']
+    },
+    {
+        input: 'set an alarm',
+        output: ['alarm']
+    }
+
+]
 
     // just check chatbot input output content on console 
-    const list = chatbotIO.filter(io => {return io;});
-    console.log(list);
+    const list = chatbotIO.filter(io => io);
 
     // random fuction, every time show rondom answer
-    const random = (io) => { return io[0].output[Math.floor(Math.random()*3)];}
+    const random = (io) => io[0].output[Math.floor(Math.random()*3)];
     // shortest function, every time show shortest answer
-    const shortest = (io) =>{ return io[0].output.reduce((first, second) => first.length <= second.length ? first : second)}
+    const shortest = (io) => io[0].output.reduce((first, second) => first.length <= second.length ? first : second);
     // longest function, every time show longest answer
-    const longest = (io) =>{ return io[0].output.reduce((first, second) => first.length >= second.length ? first : second)}
+    const longest = (io) => io[0].output.reduce((first, second) => first.length >= second.length ? first : second);
 
     // comparison of two values(user input and our input) and call push content function
 function reply(radioButton){
     const questionValue = document.querySelector("#input").value;
+    let filterOutput = "";
     // i checked that if array input value is equal to our input value 
     const filterIO = chatbotIO.filter(ios => { 
-        console.log("ios", ios.input);
-        return ios.input.includes(questionValue.toLowerCase()); 
+        if(ios.input.includes(questionValue.toLowerCase())){
+        filterOutput = ios.output[0];
+        return ios.input.includes(questionValue.toLowerCase());
+        }
+         
     })
-    console.log("filterIO",filterIO);
-    // response minumum 1 object if input type is inside the array. After i call pushContent() function which button clicked.
-    if(filterIO.length === 1){
+    // first i call function if output is dog or alarm. and response minumum 1 object if input type is inside the array. After i call pushContent() function which button clicked.
+    if(filterOutput === "dog"){
+        showDogRandom();
+    } else if(filterOutput === "alarm"){
+        setAnAlarm();
+    } else if(filterIO.length === 1){
             if(radioButton === "longest"){
                 pushContent(questionValue,longest(filterIO)); 
             } else if(radioButton === "shortest"){
@@ -43,36 +58,48 @@ function reply(radioButton){
             } else {
                 pushContent(questionValue,random(filterIO));
             }
-        }
-    else {
+    } else {
         const undefinedContent = "I don't understand that command. Please enter another command.";
         pushContent(questionValue, undefinedContent);
-        console.log(undefinedContent);
     }    
 }
 
 // Push and storage every content for user and chatbot.
 function pushContent(questionValue, chatbotValue){
     const selectedOutput =  document.querySelector("#output");
-    const storageOutput = [];
-    storageOutput.push(`You: ${questionValue}`,`Chatbot: ${chatbotValue}`);
-    for(let  i = 0; i < storageOutput.length; i++ ){
-        selectedOutput.textContent += `${storageOutput[i]}\n`;
-    }
+    selectedOutput.textContent += `You: ${questionValue}\nChatbot: ${chatbotValue}\n\n`;
 }
 
 // run reply function after click submit method and control which button clicked.
 document.querySelector("#submit").addEventListener("click", function(){
 
     if(document.getElementById("longest").checked){
-        console.log("longest");
         reply("longest");
     } else if(document.getElementById("shortest").checked){
-        console.log("shortest");
         reply("shortest");
     } else{
-        console.log("random");
         reply("random");
     }
     
 })
+
+// Get dog images from API and set image source with url which from came API
+function showDogRandom(){
+    console.log("show dog");
+    let getImage = new XMLHttpRequest();
+    getImage.onreadystatechange = function(){
+        if(getImage.readyState == XMLHttpRequest.DONE){
+            const data = JSON.parse(getImage.responseText).message;
+            const imageElement = document.querySelector("#dog-image");
+            const randomImageElement = document.querySelector(".random-dog-image");
+            imageElement.setAttribute("style","display:inline");
+            randomImageElement.src = data;
+        }
+    }
+    getImage.open('GET', 'https://dog.ceo/api/breeds/image/random', true);
+    getImage.send();
+}
+// show alert after write ser an alarm
+function setAnAlarm(){
+    alert("Did you forget about me? It’s your friend, the Alarm!");
+}
