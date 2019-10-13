a73ef8ff3b52874acecbbb2802266cf8cef8ff8f
iwaseem <52173678+iwaseem@users.noreply.github.com>
2019-10-13 11:06:43 -0400

    Add files via upload

--- README.md
+++ README.md
@@ -1,12 +1,25 @@
-# Chatbot
+Welcome to Glitch
+Click Show in the header to see your app live. Updates to your code will instantly deploy and update live.
 
-Please see the instructions [here](https://docs.google.com/document/d/1BP27Tjgit66o6kLpzu8RMnPr7M5GWpC_2N05DvMMTUE/).
+Glitch is the friendly community where you'll build the app of your dreams. Glitch lets you instantly create, remix, edit, and host an app, bot or site, and you can invite collaborators or helpers to simultaneously edit code with you.
 
+Find out more about Glitch.
 
-# Chatbot - week 10 
+Your Project
+← README.md
+That's this file, where you can tell people what your cool website does and how you built it.
 
-Please see the instructions [here](https://docs.google.com/document/d/1qrdVIXy7D10M3AkQUIKV0z7kkIOoCsofMD3ZJexyy2s/edit?usp=sharing)
+← index.html
+Where you'll write the content of your website.
 
-# Chatbot - week 11 
+← style.css
+CSS files add styling rules to your content.
 
-Please see the instructions [here](https://docs.google.com/document/d/1sWx_g90eLzSyfLRNfeZzCYUGqR5ArW4LT10D2dWctCU/edit?usp=sharing)
+← script.js
+If you're feeling fancy you can add interactivity to your site with JavaScript.
+
+← assets
+Drag in assets, like images or music, to add them to your project
+
+Made by Glitch
+\ ゜o゜)ノ
--- script.js
+++ script.js
@@ -1,92 +1,114 @@
-const chatbot = [
-    {
-        input:  ['hello', 'hi', 'greetings'],
-        output: ['hello', 'hey', 'greetings']
-    },
-    {
-        input:  ['how are you?', 'how is the weather today?', 'how is Canada doing in the Olympics?'],
-        output: ['fine', 'great', 'not so good']
-    },
-    {
-        input:  ['what is your favourite colour?', 'who is your favourite HYF instructor?', 'who is your role model?'],
-        output: ['i am not sure', 'i have so many favorites it\'s hard to choose one.', 'i like every one']
-    },
-    {
-        input: ['show me a dog'],
-        output: [showMeADog]
-    },
-    {
-        input: ['set an alarm'],
-        output: [delayedAlert]
+function showMeADog(){
+  //get a dog image from API
+  let dataRequest = new XMLHttpRequest();
+  dataRequest.onreadystatechange = function(){
+    if(dataRequest.readyState == 4){
+      //change the returned data from API to Object, and get the url from .message
+      let imageUrl = JSON.parse(dataRequest.responseText).message;
+      //change img attr -src- to the url from API
+      document.querySelector('#show-content img').setAttribute('src', imageUrl);
+      //remove the class hide from the element to show the image
+      document.getElementById('show-content').classList.remove('hide');
     }
+  }
+  //the url for API ,, and then send the request
+  dataRequest.open('GET', 'https://dog.ceo/api/breeds/image/random', true);
+  dataRequest.send();
+}
 
+function delayedAlert(){
+  setTimeout(function(){
+    alert("Did you forget about me? it's your friend, the Alarm!")
+  }, 2000);
+}
+
+const chatbot = [
+  {
+      input: ['Hello', 'Hi', 'Greetings'],
+      output: ['Hello', 'Hi', 'Greetings']
+  },
+  {
+      input: ['how is class', 'how are you enjoying class', 'how do you like class'],
+      output: ['Fantastic', 'Amazing', 'Bad']
+  },
+  {
+      input: ['do you like to code?', 'do you like to code', 'do you like css'],
+      output: ['I am not sure', 'Not at all', 'I love it']
+  },
+  {
+      input: ['show me a dog'],
+      output: [showMeADog]
+  },
+  {
+      input: ['set an alarm'],
+      output: [delayedAlert]
+  }
+
+  {
+      input: ['Show Me The Weather'],
+      output: [ShowMeTheWeather]
+  }
 ];
 
-
-
-function reply() {
+const sendResponse = () => {
     let randomNumber = Math.floor((Math.random() * 3));
-    let question = document.getElementById('input').value();
-    let filterType = null;
-    const output = chatbot.filter(item => item.input.includes(question));
+    let question = document.getElementById('user-input').value.toLowerCase();
 
     if(question === ''){ return false; }
-    appendToOutput(question);
+    appendToOutput(question, false);
 
-    if(output.length > 0){
-        if(document.getElementById('longest').checked){
-            long = output[0].output.sort((a, b) => b.length - a.length);
-            appendToOutput(long[0], 1);
-        }else if(document.getElementById('shortest').checked){
-            short = output[0].output.sort((a, b) => a.length - b.length );
-            appendToOutput(sOutput[0], 1);
-        }else{
-            (output[0]['output'].length > 1)? appendToOutput(output[0].output[randomNumber], 1):appendToOutput(output[0].output[0], 1);
-        }
-    }else{
-        //if the question not found in out dataDase show this msg to the user
-        appendToOutput("I don't understand that command. Please enter another.",1);
+    const output = chatbot.filter(item => item.input.map(item => item.toLowerCase()).includes(question));
+
+    if (output.length > 0){
+      if (document.getElementById('longest').checked) {
+        const longestOutput = output[0].output.sort((a, b) => b.length - a.length);
+        appendToOutput(longestOutput[0], true);
+      } else if(document.getElementById('shortest').checked) {
+        const shortestOutput = output[0].output.sort((a, b) => a.length - b.length );
+        appendToOutput(shortestOutput[0], 1);
+      } else{
+        //check if the output array has more than value,, if yes get a random one else get the first one
+        (output[0]['output'].length > 1)
+          ? appendToOutput(output[0].output[randomNumber], 1)
+          :appendToOutput(output[0].output[0], 1);
+      }
+    } else {
+      appendToOutput("I don't understand that command. Please enter another.", true);
     }
 }
 
-function appendToOutput(msg, sender) {
-    if (typeof msg == 'function') {
-        msg();
+const appendToOutput = (msg, sender) => {
+    if (msg instanceof Function) {
+        msg();//call the function ,, which got from the output
 
+        //add an answer on the chat history
         appendToOutput("With pleasure", 1)
         return false;
     }
-    sender = (sender) ? 'ChatBot':'You';
-    let newLine = (sender === 'ChatBot')? '\n':'\n\n';
-    document.getElementById('output').textContent = sender + ' : ' + msg + newLine + document.getElementById('output').textContent;
+    sender = (sender) ? 'Bot':'You';
+    document.getElementById('output').textContent = sender + ' : ' + msg +'\n'+ document.getElementById('output').textContent;
 }
-
-
-
-document.querySelector('.close').addEventListener('click', function(e) {
-    document.getElementById('show-content').classList.add('hide');
-});
 
 document.getElementById('chatbot-form').addEventListener('submit', function(e) {
     e.preventDefault();
-    reply();
+    sendResponse();
     document.getElementById('chatbot-form').reset();
-});
 
+})
 
-let image = new XMLHttpRequest();
-image.onreadystatechange = function() {
-   if (image.readyState == 4) {
-	//alert(http.responseText);
-  let url = JSON.parse(image.responseText).message;
-   }
+// async function
+async function ShowMeTheWeather() {
+  // await response of fetch call
+  let response = await fetch('http://api.openweathermap.org/data/2.5/forecast?q=TORONTO,CA&APPID={65fb059755e7e579db81a98b5956bba7
+}');
+// only proceed once promise is resolved
+let data = await response.json();
+// only proceed once second promise is resolved
+return data;
 }
+// trigger async function
+// log response or catch error of fetch promise
 
-variable1.open('GET', 'https://dog.ceo/api/breeds/image/random', true);
-variable1.send(null);
-
-
-//set timout for 2 seconds
-function delayAlert(){
-    setTimeout(function(){alert("Did you forget about me? it's your friend, the Alarm!")}, 2000);
-}
+fetchAsync()
+    .then(data => console.log(data))
+    .catch(reason => console.log(reason.message))
