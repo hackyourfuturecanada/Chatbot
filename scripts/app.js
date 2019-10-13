875e61741ed10b2212f0ff182e7f2d5384293470
Umit Ilhan <umitilhan@Umits-MacBook-Pro-2.local>
2019-10-13 15:29:48 -0400

    update with weather api

--- scripts/app.js
+++ scripts/app.js
@@ -2,15 +2,15 @@
 const chatbotIO = [
     {
         input :  ['hello', 'hi', 'greetings'],
-        output : ["Hello", "Hi", "Hey"]
+        output : ['Hello', 'Hi', 'Hey']
     },
     {
         input  : ['what is your favourite colour?', 'who is your favourite HYF instructor?', 'who is your role model?'],
-        output : ["I am not sure", "I like every color", "Orange"]
+        output : ['I am not sure', 'I like every color', 'Orange']
     },
     {
         input : ['how are you?', 'how is the weather today?', 'how is canada doing in the olympics?'],
-        output : ["Great", "Cool", "Good"]
+        output : ['Great', 'Cool', 'Good']
     },
     {
         input : 'show me a dog',
@@ -19,6 +19,10 @@ const chatbotIO = [
     {
         input: 'set an alarm',
         output: ['alarm']
+    },
+    {
+        input: 'show me the weather',
+        output: ['getWeather']
     }
 
 ]
@@ -35,8 +39,8 @@ const chatbotIO = [
 
     // comparison of two values(user input and our input) and call push content function
 function reply(radioButton){
-    const questionValue = document.querySelector("#input").value;
-    let filterOutput = "";
+    const questionValue = document.querySelector('#input').value;
+    let filterOutput = '';
     // i checked that if array input value is equal to our input value 
     const filterIO = chatbotIO.filter(ios => { 
         if(ios.input.includes(questionValue.toLowerCase())){
@@ -46,53 +50,61 @@ function reply(radioButton){
          
     })
     // first i call function if output is dog or alarm. and response minumum 1 object if input type is inside the array. After i call pushContent() function which button clicked.
-    if(filterOutput === "dog"){
+    if(filterOutput === 'dog'){
         showDogRandom();
-    } else if(filterOutput === "alarm"){
+    } else if(filterOutput === 'alarm'){
         setAnAlarm();
+    } else if(filterOutput === 'getWeather'){
+        city = 'Toronto';
+        apiKey ='620ff778dc8d41a5fe7a7f168ba90d92';
+        getWeather(`http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${apiKey}`)
+        .then(response => {
+            pushContent(questionValue,`Toronto weather today is ${response.weather[0].description} and Toronto's temperature is ${Math.floor(response.main.temp-273.15)}`);
+        })
+        .catch(err=>console.log(err));
     } else if(filterIO.length === 1){
-            if(radioButton === "longest"){
+            if(radioButton === 'longest'){
                 pushContent(questionValue,longest(filterIO)); 
-            } else if(radioButton === "shortest"){
+            } else if(radioButton === 'shortest'){
                 pushContent(questionValue,shortest(filterIO)); 
             } else {
                 pushContent(questionValue,random(filterIO));
             }
     } else {
-        const undefinedContent = "I don't understand that command. Please enter another command.";
+        const undefinedContent = 'I don\'t understand that command. Please enter another command.';
         pushContent(questionValue, undefinedContent);
     }    
 }
 
 // Push and storage every content for user and chatbot.
 function pushContent(questionValue, chatbotValue){
-    const selectedOutput =  document.querySelector("#output");
+    const selectedOutput =  document.querySelector('#output');
     selectedOutput.textContent += `You: ${questionValue}\nChatbot: ${chatbotValue}\n\n`;
 }
 
 // run reply function after click submit method and control which button clicked.
-document.querySelector("#submit").addEventListener("click", function(){
+document.querySelector('#submit').addEventListener('click', function(){
 
-    if(document.getElementById("longest").checked){
-        reply("longest");
-    } else if(document.getElementById("shortest").checked){
-        reply("shortest");
+    if(document.getElementById('longest').checked){
+        reply('longest');
+    } else if(document.getElementById('shortest').checked){
+        reply('shortest');
     } else{
-        reply("random");
+        reply('random');
     }
     
 })
 
 // Get dog images from API and set image source with url which from came API
 function showDogRandom(){
-    console.log("show dog");
+    console.log('show dog');
     let getImage = new XMLHttpRequest();
     getImage.onreadystatechange = function(){
         if(getImage.readyState == XMLHttpRequest.DONE){
             const data = JSON.parse(getImage.responseText).message;
-            const imageElement = document.querySelector("#dog-image");
-            const randomImageElement = document.querySelector(".random-dog-image");
-            imageElement.setAttribute("style","display:inline");
+            const imageElement = document.querySelector('#dog-image');
+            const randomImageElement = document.querySelector('.random-dog-image');
+            imageElement.setAttribute('style','display:inline');
             randomImageElement.src = data;
         }
     }
@@ -101,5 +113,14 @@ function showDogRandom(){
 }
 // show alert after write ser an alarm
 function setAnAlarm(){
-    alert("Did you forget about me? It’s your friend, the Alarm!");
+    alert('Did you forget about me? It’s your friend, the Alarm!');
 }
+
+// get Weather from openweathermap api
+
+async function getWeather(url){
+    let response = await fetch(url);
+    let data = await response.json();
+    return data;
+}
+
