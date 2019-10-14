ff1701d13bda3ff1c571a62c12868cc4282a7916
esyasar <elifsueyasar@gmail.com>
2019-10-14 11:48:05 -0400

    chatbot part5 and corrections

--- chatbot2.js
+++ chatbot2.js
@@ -1,7 +1,7 @@
 //I declared variable for users inputs
  //and, each input have three possible output.
 
- var firstData = [
+ const firstData = [
    { input: ['hello', 'hi', 'greetings'],
      output : ["Hey", "Hello", "Greetings"]
    },
@@ -20,15 +20,16 @@
 
    { input: "set an alarm",
      output: [delayedAlert]
-   }
+   },
+   {
+      input: ["show me the weather"],
+      output: [showMetheWeather]
+    }
  ];
 
 function reply(){
   // this variable taking the input from what user type and make it lowercase
   var question = document.querySelector("#input").value.toLowerCase();
-
-  // this variable will store the answers from the chatBot
-  var history = document.querySelector("#output");
 
   // this is for to choose random number from 0 to 2
   var randomNumber =  Math.floor((Math.random() * 3 ));
@@ -37,7 +38,7 @@ function reply(){
   var filterType = null;
 
 // this variable searching the input arrays for possible answers.
-const filtered = firstData.filter((item) => item.input.includes(question));
+const output = firstData.filter((item) => item.input.includes(question));
 
 /*  I declared a conditional statement to check if users input and data input are matching or not.
 If they are matching, it will give the possible answer depends on which radio button that user clicked. */
@@ -56,32 +57,60 @@ if(output.length > 0){
 }else{
   addToOutput("I don't understand that command. Please enter another.", 1);
 }
+ }
 
 function addToOutput(msg, sender){
   sender = (sender) ? 'Bot':'You';
   document.getElementById('output').textContent = sender + ' : ' + msg + '\n' + document.getElementById('output').textContent;
 }
-  }
 //  attached a 'click' event listener to button to call function.
 document.getElementById("submit").addEventListener("click", function(){
   reply()
 });
 
 function showMeaDog(){
- var image = new XMLHttpRequest();
-  image.onreadystage = function() {
-    if (image.readyStage === 4){
-      const data = JSON.parse(image.responseText);
+ var xhr = new XMLHttpRequest();
+  xhr.onreadystage = function() {
+    if (xhr.readyStage === 4){
+      const data = JSON.parse(xhr.responseText);
 
       document.getElementById("apiImage").setAttribute('src', data.message)
     }
   }
-  image.open('GET', "https://dog.ceo/api/breeds/image/random", true);
-  image.send(null);
+  xhr.open('GET', "https://dog.ceo/api/breeds/image/random", true);
+  xhr.send(null);
 }
 
 // function to show a message after 2 seconds
 function delayedAlert(){
   setTimeout( function(){
     alert("Did you forget about me? It’s your friend, the Alarm!");},2000);
+}
+var getWeather = function(){
+  return new Promise(function (resolve, reject) {
+	  var request = new XMLHttpRequest();
+    request.onreadystatechange = function() {
+       if (request.status >= 200 && request.status < 300) {
+         resolve(JSON.parse(request.response));
+       } else {
+         reject({
+           status: request.status,
+           statusText: request.statusText
+         });
+       }
+     }
+    request.open('GET', 'https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=7a1f7ab6d03f41539f5d31e1f471c654', true );
+    request.send();
+})then(function(data){
+    document.getElementById('cc').textContent = data.capital;
+  })
+};
+
+async function showMetheWeather(){
+  try {
+    const weatherData = await getWeather();
+    addToOutput(weatherData.output, 1);
+  }catch(error){
+    addToOutput(error, 1)
+  }
 }
